ff90be8f5e4cd84c14588351ca35275312227e3b
Nick Benson <nk.benson@hotmail.com>
2021-03-19 13:59:54 +0000

    complete Heroku setup

--- db/data/index.js
+++ db/data/index.js
@@ -3,11 +3,10 @@ const ENV = process.env.NODE_ENV || "development";
 const testData = require("./test-data");
 const developmentData = require("./development-data");
 
-// NB: if there's an issue in this file, check whether the requires above need destructuring
-
 const data = {
   development: developmentData,
   test: testData,
+  production: developmentData,
 };
 
 module.exports = data[ENV];
--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,5 +1,18 @@
+const ENV = process.env.NODE_ENV || "development";
 const knex = require("knex");
-const customConfig = require("../knexfile");
+const customConfig =
+  ENV === "production"
+    ? {
+        client: "pg",
+        connection: {
+          connectionString: process.env.DATABASE_URL,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    : require("../knexfile");
+
 const dbConnection = knex(customConfig);
 
 module.exports = dbConnection;
